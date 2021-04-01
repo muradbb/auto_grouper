77dda3c960ccf4b2221231143d31eec995ce5792
Murad Baghirov <mbaghirov@aris.az>
2021-04-01 17:19:30 +0400

    Finished the basic functionality

--- background.js
+++ background.js
@@ -1,21 +1,36 @@
-function urlTrimmer(url){
-  let re=/(?<=\/\/).*?(?=\/)/
-  let matched=re.exec(url)
-  return matched;
+function urlTrimmer(url) {
+    let re = /(?<=\/\/).*?(?=\/)/
+    let matched = re.exec(url)
+    return matched;
 }
 
-chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-  var groupingMap = new Map()
+chrome.tabs.query({
+    windowId: chrome.windows.WINDOW_ID_CURRENT
+}, (tabs) => {
+    var groupingMap = new Map()
     for (var i = 0; i < tabs.length; i++) {
-      if(tabs[i].groupId==-1){
-        if(!groupingMap.has(urlTrimmer(tabs[i].url)[0])){
-          groupingMap.set((urlTrimmer(tabs[i].url)[0]),[tabs[i]])
-        }else{
-          groupingMap.get((urlTrimmer(tabs[i].url)[0])).push(tabs[i])
+        if (tabs[i].groupId == -1) {
+            if (!groupingMap.has(urlTrimmer(tabs[i].url)[0])) {
+                groupingMap.set((urlTrimmer(tabs[i].url)[0]), [tabs[i]])
+            } else {
+                groupingMap.get((urlTrimmer(tabs[i].url)[0])).push(tabs[i])
+            }
         }
-      }
     }
-      console.log(groupingMap);
-  }
-)
-// console.log("stuff");
+
+
+    groupingMap.forEach((tabsArr, key) => {
+        console.log(tabsArr);
+        console.log(key);
+        if (tabsArr.length > 1) {
+            let ids = new Array()
+            for (var i = 0; i < tabsArr.length; i++) {
+                ids.push(tabsArr[i].id)
+            }
+            chrome.tabs.group({
+                tabIds: ids
+            })
+        }
+    });
+
+})
