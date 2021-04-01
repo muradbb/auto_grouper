7b33188630e03b24f1f5ddddc1615872f7af3b04
Murad Baghirov <mbaghirov@aris.az>
2021-04-01 16:35:47 +0400

    Finished grouping tabs within a map

--- background.js
+++ background.js
@@ -1,4 +1,21 @@
-chrome.tabs.query({currentWindow:true},tabs =>{
-    console.log(tabs)
+function urlTrimmer(url){
+  let re=/(?<=\/\/).*?(?=\/)/
+  let matched=re.exec(url)
+  return matched;
+}
+
+chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+  var groupingMap = new Map()
+    for (var i = 0; i < tabs.length; i++) {
+      if(tabs[i].groupId==-1){
+        if(!groupingMap.has(urlTrimmer(tabs[i].url)[0])){
+          groupingMap.set((urlTrimmer(tabs[i].url)[0]),[tabs[i]])
+        }else{
+          groupingMap.get((urlTrimmer(tabs[i].url)[0])).push(tabs[i])
+        }
+      }
+    }
+      console.log(groupingMap);
   }
 )
+// console.log("stuff");
